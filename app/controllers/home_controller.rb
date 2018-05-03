6712836812a0391b7ab29432cbd520bb96222b94
EC2 Default User <ec2-user@ip-172-31-16-110.us-east-2.compute.internal>
2018-05-03 11:41:28 +0000

    끝

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,26 +1,23 @@
 class HomeController < ApplicationController
   def index
-    post= Post.all
+    @post= Post.all
   end
 
   def create
     @post = Post.new
-    @post.title = params[:post_content]
-    @post.content = params[:post_title]
+    @post.title = params[:post_title]
+    @post.content = params[:post_content]
     @post.save 
-    
-    redirect_to 'home/index'
+    redirect_to '/home/index'
   end
 
   def new
   end
   
   def destroy 
-    post = Post.find(params[:post_id])
+    post = Post.find(params[:id])
     post.destroy
-    
     redirect_to '/home/index'
-    
   end
   
   def edit 
@@ -32,8 +29,7 @@ class HomeController < ApplicationController
     post.title = params[:post_title]
     post.content = params[:post_content]
     post.save 
-    
     redirect_to '/home/index'
   end
-  
 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  root 'home/index'
+  root 'home#index'
+  
   get 'home/index'
-
+  
   post 'home/create'
-
-  post 'home/new'
+  
+  get 'home/new'
   
   get 'home/destroy/:id' => 'home#destroy'
   
   get 'home/edit/:id' => 'home#edit'
   
   post 'home/update/:id' => 'home#update'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  
 end
